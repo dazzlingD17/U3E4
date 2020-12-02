17e0c4f0abedf545aef7d68ac9f8a1f4857fc383
dazzling diaz <71990206+dazzlingD17@users.noreply.github.com>
2020-12-02 16:13:55 -0600

    Create Nodo.java

---
+++ Ejercicio4/Ejercicio4/Nodo.java
@@ -0,0 +1,13 @@
+package Ejercicio4;
+
+public class Nodo {
+
+    int dato;
+    Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+}
